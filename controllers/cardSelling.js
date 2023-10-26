8ddfdc28390746763635ea676163a78ce57eaa19
waliyullahi1 <waliuwaheed2021@gmail.com>
2023-10-26 09:38:30 -0700

    update

--- controllers/cardSelling.js
+++ controllers/cardSelling.js
@@ -6,7 +6,7 @@ const handletransaction = require('./transaction')
 const axios = require("axios");
 const { format, parseISO } = require('date-fns')
 const nodemailer = require('nodemailer');
-
+const sendmessage =require('./sendmessage')
 async function refrenceId() {
   try {
     const response = await axios.get('http://worldtimeapi.org/api/timezone/Africa/Lagos');
@@ -85,75 +85,22 @@ const sellingcardPin = async (req, res) => {
       
     if (examType==="WAEC" || examType==="NECO" || examType==="NABTEB") {
       const tran = await handletransaction( arrangedate, foundUser._id, time, amount, foundUserBal, `Scratch card`, foundUser.phone, `Dear Customer, You have successfully Buy ${numCodes} ${examType} . And the pin has been sent to this email ${email} `, "successful",dateOftran, `${examType} result checker`)
-   
+      
+      const email = await sendmessage(email, codes)
     } else {
       const tran = await handletransaction(arrangedate, foundUser._id, time, amount, foundUserBal, "Exam Pin", foundUser.phone, `Dear Customer, You have successfully Buy ${numCodes} ${examType} . And the pin has been sent to this email ${email} `, "successful", dateOftran, `${examType} Exam pin`)
-   
+      const email = await sendmessage(email, codes)
     }
     foundUser.walletBalance = foundUserBal 
     const result = await foundUser.save() 
 
-    let pinHtml = codes.map((pin, index) => `
-  <tr style ="margin: 10px 40px; font-family: arial, sans-serif; width: 1000px">
-    <td style="  font-size:18px; " >${index + 1}</td>
-    <td style="  font-size:18px;">${pin.name}</td>
-    <td style="  font-size:18px; ">${pin.pin}</td>
-    <td style="  font-size:18px; ">${pin.seriaNo}</td>
-  </tr>
-`).join('');
-    let transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: 'waliuwaheed2021@gmail.com', 
-        pass: 'onxr sqvu qtwa eblk'
-      }
-    });
-
-    let mailOptions = {
-      from: '"no-reply"info@abaniseedu.com', 
-      to: email, 
-      subject: 'Your Purchase on abanise ', 
-
-    html: `
-     
-    <div style="width:80%;  display: flex; justify-self: center; border: 2px solid rgb(0, 71, 0);  border: 2px;   margin: 90px 20px; ">
-    <div>
-     <div class=" rappersw" style=" background-color:rgb(0, 71, 0);  border: 12px solid rgb(0, 71, 0); text-align: center; width: 100%; margin: 10px 20px;   color: white;">
-       <h1  style="font-size:25px; text-align: center; font-weight:500;">abaniseedu</h1>
-       <p style="font-size:15px; text-align: center; font-weight:500; ">YOUR TRANSACTION DETAILS ARE AS FOLLOWS:</p>
-       <p   style="font-size:15px; text-align: center; font-weight" class=" ">Your Successful purchased  Pin</p>
-       </div>
-     <p style="font-size:15px; padding-left: 40px;  margin-left: 20px;">look the link below to view the entire pin you have purchase</p>
-     <div style="padding-left: 40px; padding-top:40px:  margin-left: 20px;">
-        <table style="width:100%;  text-align: center;" >
-                 <tr style="">
-                   <th class="cols">No </th>
-                   <th>ExamType</th>
-                   <th>Pin</th>
-                  
-                   <th>Seria no</th>
-                 </tr>
-                 <tbody >
-                 ${pinHtml}
-               </tbody>
-               </table>
-     </div>
-     <p style=" margin-top: 20px; ">If you encounter any issue while using this PIN, please send a mail to  within 7 days from now.</p>
-       <div class=" rappersw" style="   background-color: red;   text-align: center; width: 100%; margin: 10px 20px;   color: white;">
-       <h1  style="font-size:20px; text-align: center; font-weight:500;">Thanks for your Subscription</h1>
-       </div>
-    </div>
  
- </div>
-    `
-     };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) return res.status(500).send('Error sending email.');
-      return res.status(200).json({ sucess: " If an account with that email exists, a password reset link has been sent.  " });
-    });
+    
 
-      console.log(codes, 'thanks you ');
+ 
+
+      
       res.json(codes);
       for (let code of codes) {
         await Card_pin.deleteOne({ _id: code._id });
--- controllers/sendmessage.js
+++ controllers/sendmessage.js
@@ -1,40 +1,70 @@
 const nodemailer = require('nodemailer');
-const User = require('../model/Users'); 
-const jwt = require('jsonwebtoken');
 
-const sendmessage = async (req, res) => {
-    const { email, attachment } = req.body;
-    
+
+
+const sendmessage = async (email, codes) => {
   
-    if (email || attachment) {
+
+  let pinHtml = codes.map((pin, index) => `
+  <tr style ="margin: 10px 40px; font-family: arial, sans-serif; width: 1000px">
+    <td style="  font-size:18px; " >${index + 1}</td>
+    <td style="  font-size:18px;">${pin.name}</td>
+    <td style="  font-size:18px; ">${pin.pin}</td>
+    <td style="  font-size:18px; ">${pin.seriaNo}</td>
+  </tr>
+`).join('');
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'waliuwaheed2021@gmail.com', 
+        pass: 'onxr sqvu qtwa eblk'
+      }
+    });
+
+    let mailOptions = {
+      from: '"no-reply"info@abaniseedu.com', 
+      to: email, 
+      subject: 'Your Purchase on abanise ', 
+
+    html: `
      
-    
-      
-   // Token expires after 1 hour
-  
-      let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'waliuwaheed2021@gmail.com', 
-          pass: 'onxr sqvu qtwa eblk'
-        }
-      });
-  
-      let mailOptions = {
-        from: '"no-reply"waliuwaheed2021@gmail.com', 
-        to: email, 
-        subject: 'Scratch Card Pin', 
-        text: `Dear customer Thanks for your subscription and your can also get the pin you Buy At Abaniseedu by view the pdf `, 
-        attachment:'ttttttttttt'// plain text body
-      };
-  
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) return res.status(500).send('Error sending email.');
-        return res.status(200).json({ sucess: " If an account with that email exists, a password reset link has been sent.  " });
-      });
-    } else {
-      return res.status(400).json({ message: " enter your email " });
-    }
-  };
+    <div style="width:80%;  display: flex; justify-self: center; border: 2px solid rgb(0, 71, 0);  border: 2px;   margin: 90px 20px; ">
+    <div>
+     <div class=" rappersw" style=" background-color:rgb(0, 71, 0);  border: 12px solid rgb(0, 71, 0); text-align: center; width: 100%; margin: 10px 20px;   color: white;">
+       <h1  style="font-size:25px; text-align: center; font-weight:500;">abaniseedu</h1>
+       <p style="font-size:15px; text-align: center; font-weight:500; ">YOUR TRANSACTION DETAILS ARE AS FOLLOWS:</p>
+       <p   style="font-size:15px; text-align: center; font-weight" class=" ">Your Successful purchased  Pin</p>
+       </div>
+     <p style="font-size:15px; padding-left: 40px;  margin-left: 20px;">look the link below to view the entire pin you have purchase</p>
+     <div style="padding-left: 40px; padding-top:40px:  margin-left: 20px;">
+        <table style="width:100%;  text-align: center;" >
+                 <tr style="">
+                   <th class="cols">No </th>
+                   <th>ExamType</th>
+                   <th>Pin</th>
+                  
+                   <th>Seria no</th>
+                 </tr>
+                 <tbody >
+                 ${pinHtml}
+               </tbody>
+               </table>
+     </div>
+     <p style=" margin-top: 20px; ">If you encounter any issue while using this PIN, please send a mail to  within 7 days from now.</p>
+       <div class=" rappersw" style="   background-color: red;   text-align: center; width: 100%; margin: 10px 20px;   color: white;">
+       <h1  style="font-size:20px; text-align: center; font-weight:500;">Thanks for your Subscription</h1>
+       </div>
+    </div>
+ 
+ </div>
+    `
+     };
 
+
+     transporter.sendMail(mailOptions, (error, info) => {
+      if (error) return res.status(500).send('Error sending email.');
+      return  email    
+     });
+ 
+};
   module.exports = sendmessage;
